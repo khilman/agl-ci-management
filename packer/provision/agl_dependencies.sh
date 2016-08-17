88f6c2de009bc67aa9a27ae50660845d79f59870
Jan-Simon Möller <jsmoeller@linuxfoundation.org>
2016-08-17 20:32:17 +0200

    Prepare basebuild files in packer
    Change-Id: Ifebdc71aac690d11289bdcd4ad06a74da7d022b1
    Signed-off-by: Jan-Simon Möller <jsmoeller@linuxfoundation.org>

---
+++ packer/provision/agl_dependencies.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+DEBIAN_FRONTEND=noninteractive apt-get update && \
+    apt-get -y install gawk wget git-core diffstat unzip texinfo gcc-multilib build-essential chrpath socat \
+                       libsdl1.2-dev xterm make xsltproc docbook-utils fop dblatex xmlto autoconf automake \
+                       libtool libglib2.0-dev libarchive-dev python-git git
