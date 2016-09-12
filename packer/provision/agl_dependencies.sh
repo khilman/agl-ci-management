5f42a16fc0bb19de3d6824af557828348af1c3db
Jan-Simon Möller <jsmoeller@linuxfoundation.org>
2016-09-12 20:18:54 +0200

    Add preclone to base image.
    Change-Id: I6d8cd0b6fc3437a76cc376c11fd4fc046759206d
    Signed-off-by: Jan-Simon Möller <jsmoeller@linuxfoundation.org>

--- packer/provision/agl_dependencies.sh
+++ packer/provision/agl_dependencies.sh
@@ -4,3 +4,8 @@ DEBIAN_FRONTEND=noninteractive apt-get update && \
     apt-get -y install gawk wget git-core diffstat unzip texinfo gcc-multilib build-essential chrpath socat \
                        libsdl1.2-dev xterm make xsltproc docbook-utils fop dblatex xmlto autoconf automake \
                        libtool libglib2.0-dev libarchive-dev python-git git python python-minimal repo mc
+
+mkdir -p /opt/AGL/preclone
+cd /opt/AGL/preclone
+repo init -u https://gerrit.automotivelinux.org/gerrit/AGL/AGL-repo
+cd
