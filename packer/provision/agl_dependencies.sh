8411414c5448e5e7f788bd14389f116d32dc5491
Jan-Simon Möller <dl9pf@gmx.de>
2016-09-23 22:46:50 +0200

    Add dependencies
    Change-Id: I094c95a55d2ad72a31829e559cb405cc5396f83c
    Signed-off-by: Jan-Simon Möller <dl9pf@gmx.de>

--- packer/provision/agl_dependencies.sh
+++ packer/provision/agl_dependencies.sh
@@ -3,7 +3,8 @@
 DEBIAN_FRONTEND=noninteractive apt-get update && \
     apt-get -y install gawk wget git-core diffstat unzip texinfo gcc-multilib build-essential chrpath socat \
                        libsdl1.2-dev xterm make xsltproc docbook-utils fop dblatex xmlto autoconf automake \
-                       libtool libglib2.0-dev libarchive-dev python-git git python python-minimal repo mc
+                       libtool libglib2.0-dev libarchive-dev python-git git python python-minimal repo mc \
+                       tree rsync bzip2 xz
 
 mkdir -p /opt/AGL/preclone
 cd /opt/AGL/preclone
