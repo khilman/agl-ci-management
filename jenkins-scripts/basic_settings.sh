321265e142c56bd7499188bd6edeba5d3b2c6bad
Jan-Simon Möller <dl9pf@gmx.de>
2016-09-24 03:55:11 +0200

    Add /tmp to be tmpfs
    This makes GCC tmp files not hit the filesystem
    
    Change-Id: I873b646ec71177efb8a379fba2ed33bc307ce0af
    Signed-off-by: Jan-Simon Möller <dl9pf@gmx.de>

--- jenkins-scripts/basic_settings.sh
+++ jenkins-scripts/basic_settings.sh
@@ -50,4 +50,11 @@ cat <<EOKNOWN >  /etc/ssh/ssh_known_hosts
 vex-yul-agl-download.ci,10.30.72.8 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBNaxEOWbShvqQWqS17c123Ct8tBLBVVOPTNYpZSmwd1UKVQi9cF0QMOU7Rc479bHwzuLscvmohpGh2kP0CmHvAo=
 EOKNOWN
 
+cat <<EOFSTAB >> /etc/fstab
+
+# tmp should be tmpfs so gcc tmpfiles do not hit the disk
+none /tmp tmpfs defaults 0 0
+
+EOFSTAB
+
 # vim: sw=2 ts=2 sts=2 et :
