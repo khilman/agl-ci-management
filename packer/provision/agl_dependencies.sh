2793b9822f27d166d6a01892364dddba77893c5d
Jan-Simon Möller <jsmoeller@linuxfoundation.org>
2016-09-24 17:57:05 +0200

    Use bash instead of dash
    Change-Id: If07aba5bd7926c16d67a80b929a3f9b61d9bd912
    Signed-off-by: Jan-Simon Möller <jsmoeller@linuxfoundation.org>

--- packer/provision/agl_dependencies.sh
+++ packer/provision/agl_dependencies.sh
@@ -6,6 +6,12 @@ DEBIAN_FRONTEND=noninteractive apt-get update && \
                        libtool libglib2.0-dev libarchive-dev python-git git python python-minimal repo mc \
                        tree rsync
 
+# we have a build blocker wrt useradd - I assume it is caused by /bin/sh being dash
+# systemd: Performing useradd with
+echo "dash dash/sh boolean false" | debconf-set-selections
+DEBIAN_FRONTEND=noninteractive dpkg-reconfigure dash
+
+# Preclone the gerrit repos for speed (use with --reference=/opt/AGL/preclone)
 mkdir -p /opt/AGL/preclone
 cd /opt/AGL/preclone
 repo init -u https://gerrit.automotivelinux.org/gerrit/AGL/AGL-repo
