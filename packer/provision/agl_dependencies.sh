c87db1c5335332ea7e1ca8cafd8241da7ec22652
Jan-Simon Möller <dl9pf@gmx.de>
2016-09-24 02:46:42 +0200

    Update repo sync command and fix dependencies
    Change-Id: If91585d0ba09afb965d2bea0f2cd1ca2c891dbae
    Signed-off-by: Jan-Simon Möller <dl9pf@gmx.de>

--- packer/provision/agl_dependencies.sh
+++ packer/provision/agl_dependencies.sh
@@ -4,9 +4,10 @@ DEBIAN_FRONTEND=noninteractive apt-get update && \
     apt-get -y install gawk wget git-core diffstat unzip texinfo gcc-multilib build-essential chrpath socat \
                        libsdl1.2-dev xterm make xsltproc docbook-utils fop dblatex xmlto autoconf automake \
                        libtool libglib2.0-dev libarchive-dev python-git git python python-minimal repo mc \
-                       tree rsync bzip2 xz
+                       tree rsync
 
 mkdir -p /opt/AGL/preclone
 cd /opt/AGL/preclone
 repo init -u https://gerrit.automotivelinux.org/gerrit/AGL/AGL-repo
+repo sync
 cd
