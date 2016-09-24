e18abb183f060b18dc4b9359b7c4be86603fcf77
Jan-Simon Möller <dl9pf@gmx.de>
2016-09-24 14:59:09 +0200

    The addition to /etc/fstab must be in the provision stage
    Change-Id: I1bb0a45dab7bb38b8b79361a36941e420951d136
    Signed-off-by: Jan-Simon Möller <dl9pf@gmx.de>

--- packer/provision/basebuild.sh
+++ packer/provision/basebuild.sh
@@ -2,3 +2,10 @@
 # vim: set tw=4 sw=4 sts=4 et :
 
 # Presently nothing to do
+
+cat <<EOFSTAB >> /etc/fstab
+
+# tmp should be tmpfs so gcc tmpfiles do not hit the disk
+none /tmp tmpfs defaults 0 0
+
+EOFSTAB
