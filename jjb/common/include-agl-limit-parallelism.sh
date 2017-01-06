b0b14de17544f64331eb14e4abfd5630e1a79722
Jan-Simon Möller <jsmoeller@linuxfoundation.org>
2017-01-07 00:59:01 +0100

    Further reduce parallel builds
    Change-Id: I9a8804fd24e45f024bea36bd668261408552fc8d
    Signed-off-by: Jan-Simon Möller <jsmoeller@linuxfoundation.org>

--- jjb/common/include-agl-limit-parallelism.sh
+++ jjb/common/include-agl-limit-parallelism.sh
@@ -2,8 +2,8 @@
 # Throttle threads to 6
 cat << EOF >> conf/auto.conf
 
-BB_NUMBER_THREADS = "6"
+BB_NUMBER_THREADS = "5"
 BB_NUMBER_PARSE_THREADS = "8"
-PARALLEL_MAKE = "-j6"
+PARALLEL_MAKE = "-j5"
 
 EOF
