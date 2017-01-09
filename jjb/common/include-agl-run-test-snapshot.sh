8dcc3d9549462c2781cc71f5cd2e81b5cb72860a
Jan-Simon Möller <jsmoeller@linuxfoundation.org>
2017-01-09 13:07:54 +0100

    Run testjob
    Change-Id: Ie81981026f483e6128fbbe12a527fa178ead5a53
    Signed-off-by: Jan-Simon Möller <jsmoeller@linuxfoundation.org>

--- jjb/common/include-agl-run-test-snapshot.sh
+++ jjb/common/include-agl-run-test-snapshot.sh
@@ -26,6 +26,56 @@ lava-tool auth-add --token-file ~/.local/token https://agl-jenkins-user@porter.a
 
 cat ~/.local/token
 
+cat <<EOF > testjob.yaml
+# Your first LAVA JOB definition for a porter board
+device_type: renesas-porter-uboot
+job_name: renesas-porter-uboot
+
+protocols:
+  lava-xnbd:
+    port: auto
+
+timeouts:
+  job:
+    minutes: 15
+  action:
+    minutes: 5
+  connection:
+    minutes: 2
+priority: medium
+visibility: public
+
+# ACTION_BLOCK
+actions:
+- deploy:
+    to: nbd
+    dtb:
+      url: 'https://download.automotivelinux.org/AGL/snapshots/master/latest/porter-nogfx/deploy/images/porter/uImage-r8a7791-porter.dtb'
+    kernel:
+      url: 'https://download.automotivelinux.org/AGL/snapshots/master/latest/porter-nogfx/deploy/images/porter/uImage'
+    initramfs:
+      url: 'https://download.automotivelinux.org/AGL/snapshots/master/latest/porter-nogfx/deploy/images/porter/initramfs-netboot-image-porter.ext4.gz.u-boot'
+      allow_modify: false
+    nbdroot:
+      url: 'https://download.automotivelinux.org/AGL/snapshots/master/latest/porter-nogfx/deploy/images/porter/core-image-minimal-porter.ext4'
+    os: debian
+    failure_retry: 2
+
+
+# BOOT_BLOCK
+- boot:
+    method: u-boot
+    commands: nbd
+    type: bootm
+    prompts: ["root@porter:~"]
+    auto_login:
+      login_prompt: "login:"
+      username: root
+
+EOF
+
 #rm ~/.local/token
+
+lava-tool submit-job https://agl-jenkins-user@porter.automotivelinux.org testjob.yaml
 
 # setup 
