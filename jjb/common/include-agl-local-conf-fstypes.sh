b854c21a7e8183622814e9583795e6c15a84809a
Jan-Simon Möller <jsmoeller@linuxfoundation.org>
2017-03-05 22:49:52 +0100

    Just enable ext4.xz as IMAGE_FSTYPE to save space
    Change-Id: I817688470858863c35eca9dab1f7ddce70e3f4be
    Signed-off-by: Jan-Simon Möller <jsmoeller@linuxfoundation.org>

---
+++ jjb/common/include-agl-local-conf-fstypes.sh
@@ -0,0 +1,14 @@
+# (c) 2016 Jan-Simon Moeller dl9pf(at)gmx.de
+# License GPLv2
+
+################################################################################
+## Change FSTYPES to sane values
+################################################################################
+
+echo 'IMAGE_FSTYPES_remove = "ext4"' >> conf/local.conf
+echo 'IMAGE_FSTYPES_remove = "ext3"' >> conf/local.conf
+echo 'IMAGE_FSTYPES_remove = "tar.bz2"' >> conf/local.conf
+echo 'IMAGE_FSTYPES_remove = "tar.gz"' >> conf/local.conf
+echo 'IMAGE_FSTYPES_remove = "iso"' >> conf/local.conf
+echo 'IMAGE_FSTYPES_remove = "hddimg"' >> conf/local.conf
+echo 'IMAGE_FSTYPES += "tar.xz ext4.xz "' >> conf/local.conf
