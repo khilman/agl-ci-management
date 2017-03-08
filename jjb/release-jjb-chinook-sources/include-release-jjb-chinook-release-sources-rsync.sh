eaa5ad8a1cf18473b0ac5d6c5f299ae32ef72a48
Jan-Simon Möller <jsmoeller@linuxfoundation.org>
2017-03-08 01:11:56 +0100

    Handle creation of source tarballs via jjb
    Change-Id: Iaaccc07c92e4d72ca8df4528503834d9af8b8bbc
    Signed-off-by: Jan-Simon Möller <jsmoeller@linuxfoundation.org>

--- jjb/release-jjb-chinook-sources/include-release-jjb-chinook-release-sources-rsync.sh
+++ jjb/release-jjb-chinook-sources/include-release-jjb-chinook-release-sources-rsync.sh
@@ -10,17 +10,9 @@ echo "#######################################"
 tree -L 2
 echo "#######################################"
 echo "#######################################"
-set | grep MACHINE
-echo "#######################################"
-echo "#######################################"
-set
-echo "#######################################"
-echo "#######################################"
 
-cd repoclone/output
-
-export RSYNCDST="jenkins-slave@10.30.72.8:/srv/download/AGL/release/${RELEASE_BRANCH}/${RELEASE_VERSION}/"
-export RSYNCSRC=$(pwd)/UPLOAD/
+#export RSYNCDST="jenkins-slave@10.30.72.8:/srv/download/AGL/release/${RELEASE_BRANCH}/${RELEASE_VERSION}/"
+#export RSYNCSRC=$(pwd)/UPLOAD/
 
 # construct upload folder
 mv UPLOAD UPLOAD2 || true
@@ -28,22 +20,12 @@ rm -rf UPLOAD2 || true
 mkdir -p UPLOAD/${MACHINE}
 export DEST=$(pwd)/UPLOAD/${MACHINE}
 
-# save space ... remove the rpms
-rm -rf tmp/deploy/rpm || true
-
-rsync -avr --progress --delete tmp/deploy $DEST/
-rsync -avr --progress --delete tmp/log $DEST/
-
-cp ../../current_default.xml $DEST/${MACHINE}_repo_default.xml
-cp conf/local.conf $DEST/local.conf
-cp conf/auto.conf $DEST/auto.conf
-echo "$BUILD_URL" > $DEST/jenkins.build.url
-
 tree $DEST
 
 ls -alhR $DEST
 
-echo "would do rsync -avr -e \"ssh -o StrictHostKeyChecking=no\" $RSYNCSRC $RSYNCDST "
+
+exit 0
 
 if test x"yes" = x"$UPLOAD" ; then
    rsync -avr -e "ssh -o StrictHostKeyChecking=no" $RSYNCSRC $RSYNCDST
