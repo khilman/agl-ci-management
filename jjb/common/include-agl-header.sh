f1a042959c83bf712886863b98318159decf555d
Jan-Simon Möller <jsmoeller@linuxfoundation.org>
2017-04-24 23:09:53 +0200

    Remove --replace-all as it only works for existing entries
    Change-Id: I8e77728ab89545d284f56d8f556fa67724110fa3
    Signed-off-by: Jan-Simon Möller <jsmoeller@linuxfoundation.org>

--- jjb/common/include-agl-header.sh
+++ jjb/common/include-agl-header.sh
@@ -143,8 +143,8 @@ set -x
 #rm -rf ~/.gitconfi* || true
 
 if test ! -f ~/.gitconfig ; then 
-  git config --global --replace-all user.email "jenkins-dontreply@build.automotivelinux.org"
-  git config --global --replace-all user.name "jenkins-dontreply@build.automotivelinux.org"
+  git config --global user.email "jenkins-dontreply@build.automotivelinux.org"
+  git config --global user.name "jenkins-dontreply@build.automotivelinux.org"
 fi
 
 #git config --global user.email "jenkins-dontreply@build.automotivelinux.org"
