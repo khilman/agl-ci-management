e282b61ffa7c8a7f465d9893d610fa329785785e
Jan-Simon Möller <jsmoeller@linuxfoundation.org>
2017-05-30 22:14:30 +0200

    Release jobs for dab.
    Change-Id: I4be4521171b15025488af09226dc8e82ec6cdf7a
    Signed-off-by: Jan-Simon Möller <jsmoeller@linuxfoundation.org>

---
+++ jjb/release-jjb-dab-release/include-release-jjb-dab-release-move-sandbox.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#set -x
+set -e
+
+if test x"" = x"${RELEASE_BRANCH}"; then
+    echo "RELEASE_BRANCH invalid"
+    exit 1
+fi
+
+if test x"" = x"${RELEASE_VERSION}"; then
+    echo "RELEASE_VERSION invalid"
+    exit 1
+fi
+
+
+export REMOTESRC="/src/download/AGL/upload/ci/${RELEASE_BRANCH}/${RELEASE_VERSION}/"
+export REMOTEDST="/srv/download/AGL/release/${RELEASE_BRANCH}/${RELEASE_VERSION}/"
+
+if test x"yes" = x"$UPLOAD" ; then
+   ssh -o StrictHostKeyChecking=no jenkins-slave@10.30.72.8 mkdir -p ${REMOTEDST}
+   ssh -o StrictHostKeyChecking=no jenkins-slave@10.30.72.8 mv ${REMOTESRC}/* ${REMOTEDST}/
+   ssh -o StrictHostKeyChecking=no jenkins-slave@10.30.72.8 rm -r ${REMOTESRC}
+   ssh -o StrictHostKeyChecking=no jenkins-slave@10.30.72.8 sh -c "cd /srv/download/AGL/release/${RELEASE_BRANCH}/ ; rm latest ; ln -sf ${RELEASE_VERSION} latest ; echo ${RELEASE_VERSION} > latest.txt" || true
+fi
+
+exit 0
---
+++ jjb/release-jjb-dab-sources/include-release-jjb-dab-release-sources-rsync.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#set -x
+set -e
+#export RSYNCDST="jenkins-slave@10.30.72.8:/srv/download/AGL/release/${RELEASE_BRANCH}/${RELEASE_VERSION}/"
+#export RSYNCSRC=$(pwd)/UPLOAD/
+
+# construct upload folder
+
+
+BRANCH=${RELEASE_BRANCH}
+RELVER=${RELEASE_VERSION}
+
+rm -rf release-upload || true
+
+git clone -b ${RELEASE_BRANCH} https://gerrit.automotivelinux.org/gerrit/p/staging/release-upload.git
+cd release-upload
+
+gpg --keyserver pgp.mit.edu --recv D6DD2170
+
+ls | grep -q agl-${RELEASE_BRANCH}-${RELEASE_VERSION}.tar.bz2
+
+( gpg --verify agl-${RELEASE_BRANCH}-${RELEASE_VERSION}.tar.bz2.sig agl-${RELEASE_BRANCH}-${RELEASE_VERSION}.tar.bz2 2>&1 | grep -e "Good signature.*Jan-Simon.*Moeller.*AGL.*Release" -q && echo "gpg key verification succeeded" ) || ( echo "gpg key verification failed" && exit 1 )
+
+rm -rf ../UPLOAD || true
+mkdir -p ../UPLOAD/
+
+cp -ar agl-${RELEASE_BRANCH}-${RELEASE_VERSION}.* ../UPLOAD/
+
+export RSYNCSRC=$(pwd)/../UPLOAD
+export RSYNCDST="jenkins-slave@10.30.72.8:/srv/download/AGL/release/${RELEASE_BRANCH}/"
+
+if test x"yes" = x"${UPLOAD}" ; then
+   ssh -o StrictHostKeyChecking=no jenkins-slave@10.30.72.8 "mkdir -p ${RSYNCDST}"
+   rsync -avr -e "ssh -o StrictHostKeyChecking=no" ${RSYNCSRC}/* ${RSYNCDST}
+fi
