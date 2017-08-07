507c7c54b5609897d3cd4d51ba99351ceebf6146
Kevin Hilman <khilman@baylibre.com>
2017-08-07 16:34:02 -0700

    WIP: LAVA: create jobs using jinja2 templates
    Demonstrate how to use jinja2 templates to create LAVA YAML jobs.
    
    To run:
    
      $ cd jjb/common
      $ ./utils/create-jobs.py
    
    Currently creates a single job with hard-coded values for rpi3.
    
    TODO:
    - generalize for QEMU
    - get machine-speific values from command-line

---
+++ jjb/common/utils/create-jobs.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+import os
+import glob
+from jinja2 import Environment, FileSystemLoader
+import random
+import string
+
+nbd = True
+
+# FIXME: hard-coded for rpi3
+yocto_machine = "raspberrypi3"
+kernel_image = "uImage"
+rootfs = "agl-demo-platform-%s.ext4.xz" % yocto_machine
+initrd = "initramfs-netboot-image-%s.ext4.gz.u-boot" % yocto_machine
+dtb = "uImage-bcm2710-rpi-3-b.dtb"
+
+template_file = os.path.join("boot", "generic-uboot-tftp.jinja2")
+job_file = "testjob.yaml"
+random_id = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(8))
+
+job = {}
+job['name'] = "AGL template test job. ID %s" % random_id
+job['priority'] = 'medium'
+job['template_file'] = template_file
+job['yocto_machine'] = yocto_machine
+
+# FIXME: AGL lab device_type names are fsck'd up
+job['device_type'] = "%s-uboot" % yocto_machine
+
+url_base = "http://www.baylibre.com/pub/agl/ci/%s/" % yocto_machine
+job['kernel_image'] = kernel_image
+job['kernel_url'] = url_base + kernel_image
+job['boot_type'] = "bootm"
+
+# NBD jobs
+if nbd:
+    job['use_nbd'] = True
+    job['boot_commands'] = "nbd"
+    job['deploy_to'] = "nbd"
+    job['nbdroot_url'] = url_base + rootfs
+    job['nbd_initrd_url'] = url_base + initrd
+    job['dtb_url'] = url_base + dtb
+
+# QEMU jobs
+elif qemu:
+    qemu_args = "-cpu qemu64,+ssse3,+sse4.1,+sse4.2,+popcnt -m 1048 -soundhw hda"
+    qemu_cmdline = "console=ttyS0,115200 root=/dev/hda debug verbose"
+    job['use_qemu'] = True
+    job['boot_method'] = "qemu"
+    job['deploy_to'] = "tmpfs"
+
+def jinja_render(job):
+    env = Environment(loader=FileSystemLoader('templates'))
+    template = env.get_template(job['template_file'])
+    return template.render(job)
+
+def main():
+    with open(job_file, 'w') as f:
+        f.write(jinja_render(job))
+    print "Job written: %s" % job_file
+
+if __name__ == '__main__':
+    main()
+    
